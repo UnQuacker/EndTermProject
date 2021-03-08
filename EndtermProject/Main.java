c759e748366ec94bd12f8939e24666b4ebdec5a4
UnQuacker <77799972+UnQuacker@users.noreply.github.com>
2021-03-08 21:57:25 +0600

    Update Main.java

--- EndtermProject/Main.java
+++ EndtermProject/Main.java
@@ -1,7 +1,7 @@
 package EndtermProject;
 
 public class Main {//do I even have to explain this part?
-    public static void main(String[] args) {//sigh, fine
+    public static void main(String[] args) {//*sigh* fine
         StartingMethod start = new StartingMethod();//creating an object of the StartingMethod class
         start.start();//calling the start() method from said class
     }
