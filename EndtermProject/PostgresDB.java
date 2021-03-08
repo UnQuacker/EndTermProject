a5acd0645d6b6a3cca0d0ca20812285e49c212de
UnQuacker <77799972+UnQuacker@users.noreply.github.com>
2021-03-08 21:57:55 +0600

    Update PostgresDB.java

--- EndtermProject/PostgresDB.java
+++ EndtermProject/PostgresDB.java
@@ -1,19 +1,19 @@
 package EndtermProject;
 
-import java.sql.*;
+import java.sql.*;//importing the package to actually be able to connect to Database
 
-public class PostgresDB implements IDB {
+public class PostgresDB implements IDB {//creating class PostgresDB implementing interface IDB
     @Override
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
+    public Connection getConnection() throws SQLException, ClassNotFoundException {//overriding getConnection method
         String connectionUrl = "jdbc:postgresql://localhost:5432/EndTermProject";
         try {
             Class.forName("org.postgresql.Driver");
 
-            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "5550125Qz");
+            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "5550125Qz");//connecting to the Database
 
-            return con;
+            return con;//returning connection
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e);//exception handling when working with connection as something can go wrong
             return null;
         }
     }
