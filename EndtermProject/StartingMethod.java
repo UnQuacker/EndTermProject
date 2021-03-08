f7e522c8a8deae2cc33eb18e8acb2b5d080a5a65
UnQuacker <77799972+UnQuacker@users.noreply.github.com>
2021-03-08 21:15:04 +0600

    Update StartingMethod.java

--- EndtermProject/StartingMethod.java
+++ EndtermProject/StartingMethod.java
@@ -14,72 +14,20 @@ public class StartingMethod {
         ResultSet resultSet=null;
         PostgresDB db = new PostgresDB();
         Scanner input= new Scanner(System.in);
+        Options op = new Options();
         try{
             System.out.println("Hello, welcome to my program!");
             while (true){
                 ui.Hello_Message();
                 int option = input.nextInt();
                 if(option == 1){
-                    System.out.println("");
-                    String empty = input.nextLine();
-                    System.out.println("Please, enter your name:");
-                    String name = input.nextLine();
-                    System.out.println("Please, enter your surname:");
-                    String surname = input.nextLine();
-                    System.out.println("Please, enter your credit card information:");
-                    String cardInfo=input.nextLine();
-                    System.out.println("Please, choose your ticket by number:");
-                    ui.getTicketsList();
-                    int ticket_id = input.nextInt();
-                    connection = db.getConnection();
-                    String sql = "INSERT INTO user_info(name,surname,credit_card_info) VALUES (?,?,?)";
-                    PreparedStatement st = connection.prepareStatement(sql);
-                    st.setString(1, name);
-                    st.setString(2, surname);
-                    st.setString(3, cardInfo);
-                    st.execute();
-                    sql = "INSERT INTO ticket_info(ticket_id) VALUES(?)";
-                    st=connection.prepareStatement(sql);
-                    st.setInt(1, ticket_id);
-                    st.execute();
+                    op.option1();
                 }
                 if(option==2){
-                    System.out.println("");
-                    String empty = input.nextLine();
-                    System.out.println("Please, enter your name:");
-                    String name = input.nextLine();
-                    System.out.println("Please, enter your surname:");
-                    String surname = input.nextLine();
-                    connection = db.getConnection();
-                    String sql = "select name, surname, starting_point, destination, price\n" +
-                            "from ticket_info\n" +
-                            "inner join user_info on ticket_info.user_id = user_info.user_id\n" +
-                            "inner join bus_ticket on ticket_info.ticket_id = bus_ticket.ticket_id\n" +
-                            "where name = '"+name+"' and surname = '"+surname+"'";
-                    st = connection.createStatement();
-                    resultSet=st.executeQuery(sql);
-                    while(resultSet.next()){
-                        System.out.println("Name: "+resultSet.getString("name")+"   Surname:: "+resultSet.getString("surname")+"   Starting Point: "+resultSet.getString("starting_point")+"   Destination: "+resultSet.getString("destination")+"   Price: "+resultSet.getInt("price"));
-                    }
+                    op.option2();
                 }
                 if(option == 3){
-                    System.out.println("");
-                    String empty = input.nextLine();
-                    System.out.println("Please, enter your name:");
-                    String name = input.nextLine();
-                    System.out.println("Please, enter your surname:");
-                    String surname = input.nextLine();
-                    connection = db.getConnection();
-                    String sql = "select sum(distinct price) as sum\n" +
-                            "from ticket_info\n" +
-                            "inner join user_info on ticket_info.user_id = user_info.user_id\n" +
-                            "inner join bus_ticket on ticket_info.ticket_id = bus_ticket.ticket_id\n" +
-                            "where name = '"+name+"' and surname = '"+surname+"'";
-                    st = connection.createStatement();
-                    resultSet=st.executeQuery(sql);
-                    while(resultSet.next()){
-                        System.out.println("Total sum: "+resultSet.getInt("sum"));
-                    }
+                    op.option3();
                 }
                 if(option==4){
                     break;
